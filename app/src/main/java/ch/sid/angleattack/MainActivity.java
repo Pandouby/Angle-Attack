ca38aa9cb2ae599b126dc7fbdd2875c8958c578b
Pandouby <57749377+Pandouby@users.noreply.github.com>
2023-03-02 08:14:52 +0100

    Add logic to disply highscore and redirect to gameActivity

--- app/src/main/java/ch/sid/angleattack/MainActivity.java
+++ app/src/main/java/ch/sid/angleattack/MainActivity.java
@@ -2,13 +2,45 @@ package ch.sid.angleattack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import ch.sid.angleattack.Highscore.Highscore;
+import ch.sid.angleattack.Highscore.HighscoreHandler;
 
 public class MainActivity extends AppCompatActivity {
+    private HighscoreHandler highscoreHandler;
+    private Highscore highscore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        highscoreHandler = new HighscoreHandler();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calcButton = findViewById(R.id.startButton);
+        calcButton.setOnClickListener(v -> startGame());
+
+        highscore = highscoreHandler.getHighscore();
+
+        TextView textView = findViewById(R.id.highscoreValue);
+        textView.setText(String.valueOf(highscore.getTime() + "s"));
+    }
+
+    private void startGame() {
+        Intent i = new Intent(this, GameActivity.class);
+        Bundle bundle = new Bundle();
+        /**
+        bundle.putDouble("weight", weight);
+        bundle.putDouble("height", height);
+
+        i.putExtras(bundle);
+         **/
+        startActivity(i);
     }
 }

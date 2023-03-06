4bcf62ec72078b6a1afb7c41dee479799dc65925
Pandouby <57749377+Pandouby@users.noreply.github.com>
2023-03-06 09:50:06 +0100

    Add stop time and setting of highscore

--- app/src/main/java/ch/sid/angleattack/GameActivity.java
+++ app/src/main/java/ch/sid/angleattack/GameActivity.java
@@ -2,6 +2,7 @@ package ch.sid.angleattack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,13 +10,20 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+import ch.sid.angleattack.Highscore.HighscoreHandler;
+
 public class GameActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor rotationSensor;
     private float[] rotationMatrix = new float[9];
     private float[] orientationValues = new float[3];
+
     private double randomAngle;
+    private HighscoreHandler highscoreHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,4 +61,35 @@ public class GameActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
+    private void gameWon() {
+        Bundle bundle = getIntent().getExtras();
+        long startTime = bundle.getLong("startTime");
+
+        double duration = (LocalTime.now().getLong(ChronoField.MILLI_OF_SECOND) - startTime) / 10;
+
+        highscoreHandler.saveHighscore(duration);
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    /**
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
+            SensorManager.getOrientation(rotationMatrix, orientationValues);
+            // Convert the orientation values from radians to degrees
+            float azimuth = (float) Math.toDegrees(orientationValues[0]);
+
+            TextView textView = findViewById(R.id.currentAngle);
+            textView.setText(String.valueOf(azimuth + "°"));
+        }
+    }
+
+
+    if(azimuth >= randomAngle - 1 && azimuth <= randomAngle + 1) {
+    gameWon();
+    }
+     **/
 }

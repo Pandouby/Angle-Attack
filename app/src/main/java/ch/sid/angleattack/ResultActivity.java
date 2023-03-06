fd27057c0989d7cdfb13c5452f90082af293deea
Pandouby <57749377+Pandouby@users.noreply.github.com>
2023-03-06 14:19:01 +0100

    Design result view and add result functionality

--- app/src/main/java/ch/sid/angleattack/ResultActivity.java
+++ app/src/main/java/ch/sid/angleattack/ResultActivity.java
@@ -2,7 +2,10 @@ package ch.sid.angleattack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Button menuButton = findViewById(R.id.backToMenu);
+        menuButton.setOnClickListener(v -> backToMenu());
+
+        Bundle bundle = getIntent().getExtras();
+        double score = bundle.getDouble("score");
+
+        TextView textView = findViewById(R.id.scoreValue);
+        textView.setText(String.valueOf(score + "s"));
+    }
+
+    private void backToMenu() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
